66614adb411f94c358439af0675c3a38562aca71
Sithembiso37 <slkhumalo37@gmail.com>
2023-09-07 08:09:09 -0700

    type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program where the function is processed
+ *
+ * Return: The value should be 0 to indicate the succes of the code
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an in: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
