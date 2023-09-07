7fac4012d8b45c5e2237937a910ba27cd19b8fe4
Sithembiso37 <slkhumalo37@gmail.com>
2023-09-07 06:11:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this performs the main function
+ *
+ * Return: this should be a 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a minltiingual puzzle");
+	return (0);
+}
