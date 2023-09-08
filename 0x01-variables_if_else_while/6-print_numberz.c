3682948f6a6ef5a1e3cdd1ddb1d6c7cdc1926906
Sithembiso37 <slkhumalo37@gmail.com>
2023-09-08 14:17:21 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,25 +8,13 @@
 
 int main(void)
 {
-	putchar('0');
-	putchar('\n');
-	putchar('1');
-	putchar('\n');
-	putchar('2');
-	putchar('\n');
-	putchar('3');
-	putchar('\n');
-	putchar('4');
-	putchar('\n');
-	putchar('5');
-	putchar('\n');
-	putchar('6');
-	putchar('\n');
-	putchar('7');
-	putchar('\n');
-	putchar('8');
-	putchar('\n');
-	putchar('9');
+	int sdnum;
+
+	for (sdnum = 0; sdnum < 10; sdnum++)
+	{
+		putchar('0' + sdnum);
+	}
+
 	putchar('\n');
 
 	return (0);
