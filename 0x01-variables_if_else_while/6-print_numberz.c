2dc55475843fc6e1a4adfdbc3c7ce1b048b31499
Sithembiso37 <slkhumalo37@gmail.com>
2023-09-08 14:06:52 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point here
+ *
+ * Return: always op 0
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('\n');
+	putchar('1');
+	putchar('\n');
+	putchar('2');
+	putchar('\n');
+	putchar('3');
+	putchar('\n');
+	putchar('4');
+	putchar('\n');
+	putchar('5');
+	putchar('\n');
+	putchar('6');
+	putchar('\n');
+	putchar('7');
+	putchar('\n');
+	putchar('8');
+	putchar('\n');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+
+}
