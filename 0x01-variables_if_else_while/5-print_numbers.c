bfa3af11114d2541be59d5e85cec4fdf5fb827b9
Sithembiso37 <slkhumalo37@gmail.com>
2023-09-08 10:50:50 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/**
+ * main - entry point here
+ *
+ * Return: always op 0
+ */
+
+int main(void)
+{
+	int sdnum = 0;
+
+	while (sdnum <= 9)
+	{
+		printf("%d\n", sdnum);
+		sdnum++;
+	}
+
+	return (0);
+
+}
